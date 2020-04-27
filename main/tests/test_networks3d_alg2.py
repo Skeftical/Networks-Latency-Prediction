b4f19c13a39618478fe93b2d9248454bf0f9afde
unknown <fotissavva@hotmail.com>
2020-04-27 15:49:30 +0100

    added simple test for N3D2

---
+++ main/tests/test_networks3d_alg2.py
@@ -0,0 +1,18 @@
+import unittest
+from main.codebase.evaluator.testing_set_generator import TestingSetGenerator
+from main.codebase.models.networks3d import Networks3DAlg2
+import numpy as np
+
+class EvaluatorSetTest(unittest.TestCase):
+    def setUp(self):
+        self.ts = TestingSetGenerator(missing_value_ratio=0.3, test_set_size=100, lags=10)
+        self.N3D2 = Networks3DAlg2()
+
+    def test_fit(self):
+        self.N3D2.fit(ts.matrices,0)
+
+    def test_shape_of_mhat(self):
+        self.assertEqual(self.N3D2.predict().shape,self.ts.matrices[0].shape)
+
+if __name__ == '__main__':
+    unittest.main()
