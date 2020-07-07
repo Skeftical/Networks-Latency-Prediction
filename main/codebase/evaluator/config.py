e403745eba9f37a49fbe3c32ba5301e59bd380c3
unknown <fotissavva@hotmail.com>
2020-07-07 15:01:31 +0300

    set limit on max_iter for networks3d algorithms

--- main/codebase/evaluator/config.py
+++ main/codebase/evaluator/config.py
@@ -4,7 +4,7 @@ simple_MF_params = {'iterations':10,'lambda_f':0.8, 'lambda_x':0.8, 'rank':10, '
 vivaldi_params = {'d':3,'gamma':0.01, 'iters':100}
 pd_params = {'tau':10, 'l':0.001, 'u':1, 'eps':1e-3, 'maxit':np.inf}
 networks3d_params = {'max_iter':3, 'd_vivaldi':3, 'gamma_vivaldi':0.01, 'iters_vivaldi':50,
-                'tau':10, 'l':0.001, 'u':1, 'eps':1e-3, 'maxit':np.inf}
+                'tau':10, 'l':0.001, 'u':1, 'eps':1e-3, 'maxit':10000}
 tsmf_params = {'alpha':0.5, 'lags':LAGS, 'smoothing_level':0.2, 'optimized':False,
                         'iterations':10,'lambda_f':0.8, 'lambda_x':0.8, 'rank':10, 'gamma':0.001}
 ses_params = {'smoothing_level': 0.2, 'optimized': False, 'lags':LAGS}
